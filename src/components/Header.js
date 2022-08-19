28635d497e42a685a879e5cdd6fbd612d7545a09
caiobacode <caioodsf@gmail.com>
2022-08-19 14:53:16 -0300

    req1

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class index extends React.Component {
+class Header extends React.Component {
   render() {
     return (
       <header>
@@ -10,4 +10,4 @@ class index extends React.Component {
   }
 }
 
-export default index;
+export default Header;
