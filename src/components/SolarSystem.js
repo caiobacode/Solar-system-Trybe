e57ee7bfd17d1b3ff698d50c2892375cd3953b2d
caiobacode <caioodsf@gmail.com>
2022-08-21 18:56:01 -0300

    req 7 e 8

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -9,12 +9,12 @@ class SolarSystem extends React.Component {
       <div data-testid="solar-system">
         <Title headline="Planetas" />
         {planets.map((item) => {
-          const itemName = item.name
-          const itemImg = item.image
+          const itemName = item.name;
+          const itemImg = item.image;
           return (
             <PlanetCard key={ itemName } planetName={ itemName } planetImg={ itemImg } />
-          )}
-        )}
+          );
+        })}
       </div>
     );
   }
